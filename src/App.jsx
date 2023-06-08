b6d378116c1b96cd72815da00691f1cbfc363351
Liam M <lmcrailt4@gmail.com>
2023-06-09 00:17:06 +0100

    Convert index.html to React (Includes layout and styling but not web components).

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './../assets/logo/logo.png'
 import './App.css'
 
+// TODO: Convert web components to react components
+
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+	return (
+		<div className="outer-body">
+			<div className="outer-section-area top-area">
+				<div id="top-section-inner-wrap">
+					<img className="logo-standard" src="../assets/logo/logo.png"/>
+					<h1 className="title-heading">Pinboard for YouTube</h1>
+				</div>
+				<hr></hr>
+			</div>
+			<div className="inner-body">
+				{/* Current video */}
+				<split-heading header="Current video"></split-heading>
+				<video-card id="current-video-card" videoid="xcJtL7QggTI"></video-card>
+				<div className="current-video-options">
+					<button className="button-small">Save video</button>
+					<button className="button-small">Pin timestamp</button>
+				</div>
+				{/* My timestamps */}
+				<split-heading header="My video timestamps"></split-heading>
+				<div id="timestamp-scrollbox">
+					<ul id="timestamp-scrollbox-list">
+						<video-timestamp-list url="https://www.youtube.com/watch?v=LXb3EKWsInQ"></video-timestamp-list>
+						<video-timestamp-list url="https://www.youtube.com/watch?v=njX2bu-_Vw4"></video-timestamp-list>
+						<video-timestamp-list url="https://www.youtube.com/watch?v=AKeUssuu3Is"></video-timestamp-list>
+						<video-timestamp-list url="https://www.youtube.com/watch?v=ZjVAsJOl8SM"></video-timestamp-list>
+						<video-timestamp-list url="https://www.youtube.com/watch?v=PnvkrBXmLSI"></video-timestamp-list>
+						<video-timestamp-list url="https://www.youtube.com/watch?v=ERYG3NE1DO8"></video-timestamp-list>
+					</ul>
+				</div>
+			</div>
+			<div className="outer-section-area bottom-area">
+				<hr></hr>
+				<div id="bottom-section-inner-wrap">
+					<button style={{gridColumn: 1, gridRow: 1}} className="button-small">Options</button>
+					<button style={{gridColumn: 1, gridRow: 2}} className="button-small">Help</button>
+					<h2 className="outer-area-subtle-text">Version 1.0.0</h2>
+				</div>
+			</div>
+		</div>
+	)
+};
 
-export default App
+export default App;
