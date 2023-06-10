c037294eb1003f4fc283e57ee33274e6adad16d6
Liam M <lmcrailt4@gmail.com>
2023-06-10 20:23:00 +0100

    Implement subtle expander as react component, and integrate expander within VideoTimestampList.

---
+++ src/components/SubtleExpander/SubtleExpander.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import "./SubtleExpander.css"
+import { v4 as uuidv4 } from 'uuid';
+
+export function SubtleExpander({ "open-message": open, "close-message": close, children }) {
+	const [expanded, setExpanded] = useState(false);
+
+	let collapseID = "se-" + uuidv4();
+	let styling = "subtle-expander-symbol " + (expanded ? "rotate-close" : "rotate-open");
+
+	return (
+		<div className="subtle-expander-inner">
+			<button onClick={(x) => {
+				setExpanded(!expanded)
+			}} className="subtle-expander-button" data-bs-toggle="collapse" href={"#" + collapseID} role="button" aria-expanded="false" aria-controls={collapseID}>
+				<img className={styling} src="./../../../assets/symbols/arrow.svg"/>
+			</button>
+			<h3 className="subtle-expander-text">{expanded ? open : close}</h3>
+			<div className="collapse subtle-expander-content" id={collapseID}>
+				{children}
+			</div>
+		</div>
+	);
+}
+
+export default SubtleExpander;
--- src/components/video/VideoTimestampList/VideoTimestampList.jsx
+++ src/components/video/VideoTimestampList/VideoTimestampList.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-//import "./../../SubtleExpander/SubtleExpander.jsx" TODO
+import SubtleExpander from "./../../SubtleExpander/SubtleExpander.jsx"
 import VideoTimestamp from  "./../VideoTimestamp/VideoTimestamp.jsx"
 import VideoCard from "./../VideoCard/VideoCard.jsx"
 import "./VideoTimestampList.css"
@@ -7,9 +7,7 @@ import "./VideoTimestampList.css"
 export function VideoTimestampList({ "video-id": videoID, timestamps }) {
 	const [times, setTimes] = useState(timestamps ?? []);
 
-	/*
-		Structure of "timestamps": JSON array of objects.
-
+	/* Structure of "timestamps", which is a JSON array of objects:
 		"timestamps": [
 			{ "time": 103, "message": "Test message" },
 			{ "time": 493, "message": "Another message" },
@@ -17,15 +15,21 @@ export function VideoTimestampList({ "video-id": videoID, timestamps }) {
 		]
 	*/
 
-	let timestampItems = times.map((x) => <li><VideoTimestamp video-id={videoID} time={x.time} message={x.message}></VideoTimestamp></li>)
+	let timestampItems = times.map((x) => <li><VideoTimestamp video-id={videoID} time={x.time} message={x.message}></VideoTimestamp></li>);
 
 	return (
-		<div id="video-timestamp-list">
-			<VideoCard video-id={videoID}></VideoCard>
-			<ul>
-				{timestampItems}
-			</ul>
-		</div>
+		<>
+			<div id="video-timestamp-list">
+				<VideoCard video-id={videoID}></VideoCard>
+				<hr className="video-timestamp-list-margin"></hr>
+				<SubtleExpander open-message="Close timestamps" close-message="Expand timestamps">	
+					<ul className="video-timestamp-list-container">
+						{timestampItems}
+					</ul>
+				</SubtleExpander>
+			</div>
+			<hr className="video-timestamp-list-separator"></hr>
+		</>
 	)
 }
 
