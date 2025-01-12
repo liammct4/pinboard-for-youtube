5700dfb007868ff521d19af71216eae63bc883d2
Liam M <lmcrailt4@gmail.com>
2025-01-12 17:54:00 +0000

    Implement directory browser controls.

--- src/components/video/navigation/VideoDirectoryBrowser/VideoDirectoryBrowser.tsx
+++ src/components/video/navigation/VideoDirectoryBrowser/VideoDirectoryBrowser.tsx
@@ -4,6 +4,8 @@ import "./VideoDirectoryBrowser.css"
 import { useVideoAccess } from "../../../features/useVideoAccess";
 import { getItemFromNode, getRootDirectoryPathFromSubDirectory, IDirectoryNode, VideoDirectoryInteractionContext } from "../directory";
 import { useNotificationMessage } from "../../../features/useNotificationMessage";
+import { IconContainer } from "../../../images/svgAsset";
+import { ReactComponent as ArrowIcon } from "./../../../../../assets/symbols/arrow_sideways.svg"
 
 export function VideoDirectoryBrowser(): React.ReactNode {
 	const { videoData, root } = useVideoAccess();
@@ -45,9 +47,35 @@ export function VideoDirectoryBrowser(): React.ReactNode {
 		}
 	}, [directory]);
 
+	const slices = directoryPath.split(">");
+	const last = slices[slices.length - 1];
+
+	slices.splice(slices.length - 1, 1);
+	let accumulator = "";
+
 	return (
 		<>
-			<span>Current Directory: <i>{directoryPath}</i></span>
+			<div className="directory-navigator">
+				<button className="button-base button-small square-button" onClick={() => setDirectoryPath(getRootDirectoryPathFromSubDirectory(directory!.parent!))}>←</button>
+				<ul className="directory-navigator-slices">
+					{
+						slices.map(x => {
+							accumulator += x;
+							let directPath = accumulator;
+							
+							accumulator += ">";
+							
+							return (
+								<li key={directPath}>
+									<button className="button-base button-small" onClick={() => setDirectoryPath(directPath)}>{x}</button>
+									<IconContainer className="icon-colour-standard" asset={ArrowIcon} use-fill/>
+								</li>
+							);
+						})
+					}
+					<li>{last}</li>
+				</ul>
+			</div>
 			<VideoDirectoryInteractionContext.Provider
 				value={{
 					navigateRequest: (requester) => setDirectoryPath(getRootDirectoryPathFromSubDirectory(requester))
