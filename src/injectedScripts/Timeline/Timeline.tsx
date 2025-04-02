925e8be5be77cc8ea37f3102a16db57de2f44c69
Liam M <lmcrailt4@gmail.com>
2025-04-02 22:04:43 +0100

    Implement navigation to multiple pages with the timeline still working.

--- src/injectedScripts/Timeline/Timeline.tsx
+++ src/injectedScripts/Timeline/Timeline.tsx
@@ -2,15 +2,22 @@ import { createRoot } from "react-dom/client"
 import { TimelineApp } from "./TimelineApp";
 
 let container = document.createElement("div");
+container.id = "pfy-timeline-root";
 let root = createRoot(container);
 
 function timelineSetup() {
 	// React entry point.
-	let progressBar = document.querySelector(".ytp-chrome-bottom");
-	progressBar?.insertBefore(container, progressBar.childNodes[0]);
-
-	root.render(<TimelineApp/>);
+	if (document.querySelector("#pfy-timeline-root") == null) {
+		let progressBar = document.querySelector(".ytp-chrome-bottom");
+		progressBar?.insertBefore(container, progressBar.childNodes[0]);
+	
+		root.render(<TimelineApp/>);
+		return;
+	}
 }
 
-// TODO: Mutation observer.
-setTimeout(timelineSetup, 100);
+window.addEventListener("navigate", () => {
+	setTimeout(timelineSetup, 100);
+});
+
+timelineSetup();
